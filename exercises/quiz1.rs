232bdddd37e5a98a616f9cf923872d383b079dee
Max Kurapov <max@interledger.org>
2024-03-07 17:31:42 +0100

    chapters 1 - 6

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    let price_per_apple = if num_apples > 40 { 1 } else { 2 };
+
+    num_apples * price_per_apple
+}
 
 // Don't modify this function!
 #[test]
