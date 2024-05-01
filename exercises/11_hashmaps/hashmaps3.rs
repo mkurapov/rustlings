8de959c0b473929bf9560e69a0559f1161178839
Max Kurapov <max@interledger.org>
2024-05-01 13:29:18 +0200

    hashmaps

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -34,11 +34,32 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded by team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        let updated_team_1_score: Team = match scores.get(&team_1_name) {
+            Some(result) => Team {
+                goals_scored: result.goals_scored + team_1_score,
+                goals_conceded: result.goals_conceded + team_2_score,
+            },
+            None => Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            },
+        };
+
+        scores.insert(team_1_name, updated_team_1_score);
+
+        let updated_team_2_score: Team = match scores.get(&team_2_name) {
+            Some(result) => Team {
+                goals_scored: result.goals_scored + team_2_score,
+                goals_conceded: result.goals_conceded + team_1_score,
+            },
+            None => Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            },
+        };
+
+        scores.insert(team_2_name, updated_team_2_score);
     }
     scores
 }
